7e922d0ca473b0c5e87a2202758319f0babadb07
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2022-12-12 13:34:13 +0700

    Add new files

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "mynotescode";
+$pdo = new PDO('mysql:host='.$host.';port=8111;dbname='.$database, $username, $password);
+?>
